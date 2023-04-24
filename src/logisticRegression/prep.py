870145e7817ca42b3c9dbf76e7907bf7c42f1eca
Shahiryar Saleem <shahiryarsaleem@gmail.com>
2023-04-24 20:55:02 +0500

    added documentation to load_csv function in prep.py

--- src/logisticRegression/prep.py
+++ src/logisticRegression/prep.py
@@ -5,6 +5,21 @@ import csv
 import numpy
 
 def load_csv(file_path):
+    """
+    Load a CSV file located at the specified file path and return a dictionary containing the column names
+    and values in the file.
+
+    Args:
+        file_path (str): The path to the CSV file to load.
+
+    Returns:
+        dict: A dictionary with the following keys:
+            - "columns": a list of strings representing the column names in the CSV file.
+            - "values": a numpy array representing the values in the CSV file.
+    
+    Raises:
+        Exception: If the number of values in a row does not match the number of columns in the CSV file.
+    """
     vals = []
     cols = []
     with open(file_path, 'r') as file:
