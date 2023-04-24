2cb2b6cb29e16867b13e76a709b60bb05ed650e5
Shahiryar Saleem <shahiryarsaleem@gmail.com>
2023-04-24 20:53:58 +0500

    added function to load csv and return numpy array in prep.py

--- src/logisticRegression/prep.py
+++ src/logisticRegression/prep.py
@@ -1,3 +1,21 @@
-#TODO: function to load a csv file and return a numpy array
 #TODO: OneHotEncoding given an array (without sklearn)
 #TODO: Standardization of data given a numpy array
+
+import csv
+import numpy
+
+def load_csv(file_path):
+    vals = []
+    cols = []
+    with open(file_path, 'r') as file:
+        reader = csv.DictReader(file)
+        cols = list(reader.fieldnames)
+        n_features = len(cols)
+        
+        for row in reader:
+            observation = list(row.values())
+            if len(observation) != n_features: 
+                raise Exception("Number of values do not match the number of columns")
+            vals.append(list(row.values()))
+    vals = numpy.array(vals, dtype=float)
+    return {"columns": cols, "values":vals}
