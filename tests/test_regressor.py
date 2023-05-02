bdafb74aa28dc7cca08c7c59470e408444c82fe4
Shahiryar Saleem <shahiryarsaleem@gmail.com>
2023-05-02 23:58:33 +0500

    mutli-class test data

--- tests/test_regressor.py
+++ tests/test_regressor.py
@@ -20,3 +20,18 @@ def test_binary_classification(binary_data):
     y_pred = model.predict(X)
     assert y_pred.shape == y.shape
     assert np.all(np.isin(y_pred, [0, 1]))
+
+@pytest.fixture
+def multi_class_data():
+    X = np.array([
+        [0.3, 0.5, 0.1],
+        [0.6, 0.1, 0.9],
+        [0.2, 0.4, 0.7],
+        [0.8, 0.1, 0.3],
+        [0.4, 0.5, 0.1],
+        [0.9, 0.2, 0.7],
+        [0.5, 0.4, 0.2],
+        [0.7, 0.3, 0.5]
+    ])
+    y = np.array([0, 1, 1, 0, 0, 2, 1, 2])
+    return X, y
