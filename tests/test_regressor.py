1fe962b77b741231b78aebb7aa367aff2903e41b
Shahiryar Saleem <shahiryarsaleem@gmail.com>
2023-05-02 23:58:51 +0500

    Multiclass test

--- tests/test_regressor.py
+++ tests/test_regressor.py
@@ -35,3 +35,11 @@ def multi_class_data():
     ])
     y = np.array([0, 1, 1, 0, 0, 2, 1, 2])
     return X, y
+
+def test_multi_class_classification(multi_class_data):
+    X, y = multi_class_data
+    model = LogisticRegression(lr=0.1, num_iter=5000, fit_intercept=True, verbose=False)
+    model.fit(X, y)
+    y_pred = model.predict_one_vs_all(X, labels=np.unique(y))
+    assert y_pred.shape == (len(np.unique(y)), len(y))
+    assert np.all(np.isin(y_pred.argmax(axis=0), np.unique(y)))
