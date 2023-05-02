dfd62426424e13d35d75a875595b833678d576aa
Shahiryar Saleem <shahiryarsaleem@gmail.com>
2023-05-02 23:58:00 +0500

    Binary Classification test

---
+++ tests/test_regressor.py
@@ -0,0 +1,22 @@
+import pytest
+from logisticRegression.regressor import LogisticRegression
+import numpy as np
+
+@pytest.fixture
+def binary_data():
+    X = np.array([
+        [0.3, 0.5, 0.1],
+        [0.6, 0.1, 0.9],
+        [0.2, 0.4, 0.7],
+        [0.8, 0.1, 0.3]
+    ])
+    y = np.array([0, 1, 1, 0])
+    return X, y
+
+def test_binary_classification(binary_data):
+    X, y = binary_data
+    model = LogisticRegression(lr=0.1, num_iter=5000, fit_intercept=True, verbose=False)
+    model.fit(X, y)
+    y_pred = model.predict(X)
+    assert y_pred.shape == y.shape
+    assert np.all(np.isin(y_pred, [0, 1]))
